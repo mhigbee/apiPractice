41a04c42eded7ff312ec2a63ae7c656e45e34d4e
Matt Higbee <mhigbee@hotmail.com>
2016-06-09 20:24:46 -0600

    further progress

--- service.js
+++ service.js
@@ -2,11 +2,14 @@ angular.module('apiPractice').service('mainService', function($http,$q){
 
 
     var baseUrl = 'http://swapi.co/api/'
-
+    //example of q
     this.starWarsData = function() {
-        return $http.get(baseUrl).then(function(response){
-            return response.data;
-        })
-    }
+        var deferred = $q.defer();
+        $http.get(baseUrl).then(function(response){
+            var starWarsData = response.data;
+            deferred.resolve(starWarsData);
+        });
+        return deferred.promise;
+    };
 
-})
+});
